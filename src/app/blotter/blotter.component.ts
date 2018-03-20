71ec912b4f5d5ff04b2639312556e54c962aa338
John VanZwieten <JVanZwieten@infragistics.com>
2018-03-20 01:53:09 -0400

    specified columns

--- src/app/blotter/blotter.component.ts
+++ src/app/blotter/blotter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from "../services/data.service";
 
+import { IgxGridCellComponent } from 'igniteui-angular/grid/cell.component'
+
 @Component({
   selector: 'app-blotter',
   templateUrl: './blotter.component.html',
