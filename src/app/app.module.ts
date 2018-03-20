6f67ac66dcc73f724c04f8212f4e5f323605c6ee
John VanZwieten <JVanZwieten@infragistics.com>
2018-03-20 08:48:07 -0400

    implemented basic trend highlighting on the first column only, need to iterate on it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IgxNavigationDrawerModule, IgxNavbarModule, IgxLayoutModule, IgxRippleModule, IgxGridModule } from 'igniteui-angular/main';
+import { IgxNavigationDrawerModule, IgxNavbarModule, IgxLayoutModule, IgxRippleModule, IgxGridModule, IgxIconModule } from 'igniteui-angular/main';
 import { HomeComponent } from './home/home.component';
 import { BlotterComponent } from "./blotter/blotter.component";
 import { DataService } from './services/data.service';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HomeComponent,
-		BlotterComponent
-	],
-	imports: [
-		FormsModule,
-		BrowserModule,
-		BrowserAnimationsModule,
-		AppRoutingModule,
-		IgxNavigationDrawerModule,
-		IgxNavbarModule,
-		IgxLayoutModule,
-		IgxRippleModule,
-		IgxGridModule.forRoot()
-	],
-	providers: [DataService],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BlotterComponent
+    ],
+    imports: [
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        IgxNavigationDrawerModule,
+        IgxNavbarModule,
+        IgxLayoutModule,
+        IgxRippleModule,
+        IgxGridModule.forRoot()
+        IgxIconModule
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
